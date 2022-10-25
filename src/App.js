f4b2258385e8417b750edb460ffe420b44af60e3
Francisco Javier Barreras Cebreros <francisco.barreras203706@potros.itson.edu.mx>
2022-10-24 18:08:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
-function App() {
+import { useState, useEffect } from "react";
+const airflowAxios = axios.create({
+  baseURL: "http://20.106.154.134:8080/api/v1/",
+  headers: {
+    accept: "application/json",
+    // "Content-Type": "application/json",
+  },
+  auth: {
+    username: "airflow",
+    password: "airflow",
+  },
+});
+export default function App() {
+  const [isClick, setisClick] = useState(false);
+  useEffect(() => {
+    const handle = async () => {
+      const ok = await triggerDag("5_apitest");
+      console.log(ok);
+    };
+    handle();
+  }, [isClick]);
+  const triggerDag = (id) => {
+    return airflowAxios
+      .post(`/dags/${id}/dagRuns/`, {
+        conf: {
+          project: "test",
+        },
+        dag_run_id: "string89",
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((error) => {
+        return error;
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello </h1>
+      <h2>Test API Airflow</h2>
+      <button onClick={() => setisClick(!isClick)}>Trigger</button>
     </div>
   );
 }
-
-export default App;
